0f84fdabfdabd6c9b5ca168e864ed0b4a3ce1d4a
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-02-20 14:38:10 -0300

    Planos elaborados (#5)
    * arrumando a navegação
    
    * criando a pagina de projetos encerrados
    
    * colocando carousel na pagina dos projetos encerrados
    
    * remove arquivos desnecessarios
    
    * adicionando o carossel
    
    * criando cards e colocando as informacoes em ordem
    
    arrumando o css da pagina
    
    tocando a cor da letra para ficar legivel
    
    removendo erros no Sass
    
    Arrumando o carossel
    
    inserindo as informacoes no card do carossel
    
    * arrumando a navegação
    
    * alterando o arquivo de html para md
    
    * arrumando o permalink entre as paginas
    
    * trocando a palavra para planos
    
    * Create _color.scss
    
    * Coffe Color Palettes
    
    * Vintage Color Palettes
    
    * definindo o layout da pagina
    
    * mudando o layout do carossel
    
    * arrumando o box-shadow do carossel
    
    * Update carrossel.css
    
    * arrumando o codigo do carousels
    
    * Update carousels.css
    
    * adicionando um _includes
    
    * colocando o include
    
    * arrumando o css do carousels
    
    * organizando o codigo
    
    * adicionando o include para a lista
    
    * Create _list-encerrados.html
    
    * Create lista-encerrados.css
    
    * remove files
    
    * remove line css
    
    * arrumando a lista para a pagina
    
    * inserindo o sass para o layout da lista
    
    * mudando o arquivo da pagina
    
    * adicionando o include de apresentação da pagina
    
    * adicionando a introducao
    
    * colocando o sass na introducao
    
    * colocando um texto generico como exemplo
    
    * deleted file
    
    * row deleted
    
    * changing the file
    
    * editando o css para não dar conflito
    
    * Create _lista-elaborados.html
    
    * Update head.html
    
    * criando paginacao com js
    
    * colocando o include
    
    * criando a pagina de planos elaborados
    
    * Update accordion-carousels.css
    
    * corrigindo o codigo
    
    agora o codigo não da conflito com o css nem o html
    
    * Delete accord-carou.js
    
    * Delete img
    
    * add imgs
    
    * arruamndo o accordion da pagina
    
    * Update navegacao.js
    
    * adicionando os accordion e alterando os buttons
    
    * organizando as paginas
    
    * adicionando os icons da pagina
    
    * adicionando o estilo no accordeon e modificando a navegacao
    
    Adicionado o estilo no accordeon
    adicionando a transição entre a imagem e as informações
    arrumando a navegação entre as paginas
    arrumando o espaçamento entre um accordeon e outro
    
    * arrumando a navegacao entre as paginas
    
    * testando o accordion
    
    Resolve #5

---
+++ assets/js/navegacao.js
@@ -0,0 +1,178 @@
+const dataList = Array.from(document.querySelectorAll('.list .item')).map(item => item.innerHTML);
+
+let perPage = 4;
+const state = {
+    page: 1,
+    perPage,
+    totalPage: Math.ceil(dataList.length / perPage),
+    maxVisibleButtons: 5
+};
+const html = {
+    get(element) {
+        return document.querySelector(element);
+    }
+};
+const controls = {
+    next() {
+        state.page ++;
+        const lastPage = state.page > state.totalPage;
+        if (lastPage) {
+            state.page--;
+        };
+    },
+
+    prev() {
+        state.page--;
+
+        const firstPage = state.page < 1;
+        if (firstPage) {
+            state.page++;
+        };
+    },
+
+    goTo(page) {
+        if (page < 1) {
+            page = 1;
+        }
+
+        state.page = +page;
+        if (page > state.totalPage) {
+            state.page = state.totalPage;
+        }
+
+    },
+
+    createListeners() {
+        html.get('.first').addEventListener('click', () => {
+            controls.goTo(1);
+            update();
+        });
+
+        html.get('.last').addEventListener('click', () => {
+            controls.goTo(state.totalPage);
+            update();
+        });
+
+        html.get('.prev').addEventListener('click', () => {
+            controls.prev();
+            update();
+        });
+
+        html.get('.next').addEventListener('click', () => {
+            controls.next();
+            update();
+        });
+    }
+};
+
+const list = {
+    create(item) {
+        const div = document.createElement('div');
+        div.classList.add('item');
+        div.innerHTML = item;
+
+        html.get('.list').appendChild(div);
+    },
+    update() {
+        const { perPage } = state;
+        html.get('.list').innerHTML = '';
+
+        let page = state.page - 1;
+        let start = page * perPage;
+        let end = start + perPage;
+
+        const paginatedItems = dataList.slice(start, end);
+        paginatedItems.forEach(list.create);
+    }
+};
+
+const buttons = {
+    element: html.get('.controls .numbers'),
+    create(number) {
+        const button = document.createElement('div');
+
+        button.innerHTML = number;
+
+        if (state.page == number) {
+            button.classList.add('active');
+        }
+
+        button.addEventListener('click', (event) => {
+            const page = event.target.innerText;
+
+            controls.goTo(page);
+            update();
+        });
+
+        buttons.element.appendChild(button);
+    },
+    update() {
+        buttons.element.innerHTML = '';
+
+        const { maxLeft, maxRight } = buttons.calculateMaxVisible();
+
+        for (let page = maxLeft; page <= maxRight; page++) {
+            buttons.create(page);
+        };
+    },
+    calculateMaxVisible() {
+        const { maxVisibleButtons, totalPage } = state;
+
+        let maxLeft = (state.page - Math.floor(maxVisibleButtons / 2));
+        let maxRight = (state.page + Math.floor(maxVisibleButtons / 2));
+
+        if (maxLeft < 1) {
+            maxLeft = 1;
+            maxRight = maxVisibleButtons;
+        };
+
+        if (maxRight > totalPage) {
+            maxLeft = totalPage - (maxVisibleButtons - 1);
+            maxRight = totalPage;
+
+            if (maxLeft < 1) maxLeft = 1;
+        };
+
+        return { maxLeft, maxRight };
+    }
+};
+
+function blockButtons() {
+    const { page, totalPage } = state;
+
+    const elements = {
+        prev: html.get('.controls .prev'),
+        next: html.get('.controls .next'),
+
+        first: html.get('.controls .first'),
+        last: html.get('.controls .last')
+    };
+
+    if (page == 1) {
+        elements.prev.classList.add('disable');
+        elements.first.classList.add('disable');
+    } else {
+        elements.prev.classList.remove('disable');
+        elements.first.classList.remove('disable');
+    };
+
+    if (page == totalPage) {
+        elements.next.classList.add('disable');
+        elements.last.classList.add('disable');
+    } else {
+        elements.next.classList.remove('disable');
+        elements.last.classList.remove('disable');
+    };
+}
+
+function update() {
+    list.update();
+    buttons.update();
+    controls.createListeners();
+    blockButtons();
+};
+function init() {
+    update();
+};
+
+window.addEventListener('load', init);
---
+++ pages/encerrados.md
@@ -0,0 +1,14 @@
+---
+layout: page
+title:  "Planos Encerrados"
+categories: Projetos
+permalink:  /encerrados/
+---
+
+<div>
+{% include _list-encerrados.html %}
+</div>
+
+<div>
+{% include _carousels.html %}
+</div>
